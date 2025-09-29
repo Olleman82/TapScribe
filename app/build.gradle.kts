29de807c703b326b1e8669a5c63d8e712b8fcdb4
Olleman82 <olle.soderqvist@gmail.com>
2025-09-29 20:49:36 +0200

    chore: bump version for Play Store release (versionCode 4, versionName 0.3.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ android {
   compileSdk = 35
 
   defaultConfig {
-    applicationId = "se.olle.rostbubbla.welcome4"
+    applicationId = "se.olle.rostbubbla"
     minSdk = 26
     targetSdk = 35
-    versionCode = 1
-    versionName = "0.1.0"
+    versionCode = 4
+    versionName = "0.3.1"
 
     vectorDrawables { useSupportLibrary = true }
   }
